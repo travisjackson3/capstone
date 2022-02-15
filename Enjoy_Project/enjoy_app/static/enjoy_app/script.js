b82a34e6cce9c27d1f74658889a0c0d628f23305
travisjackson3 <travisjackson3@gmail.com>
2022-02-14 19:55:41 -0800

    progress

--- Enjoy_Project/enjoy_app/static/enjoy_app/script.js
+++ Enjoy_Project/enjoy_app/static/enjoy_app/script.js
@@ -4,12 +4,41 @@
 var image = new Image();
 
 const testClick = document.querySelectorAll('.testclick');
-let canvas = document.getElementById("canvas");
+let canvas = document.getElementById("colorCanvas");
 let ctx = canvas.getContext("2d");
 let testchange = document.getElementById("testchange")
 
-ctx.fillStyle ="white";
+
+console.log(canvas.width)
+console.log(canvas.height)
+
+
+let rect = canvas.getBoundingClientRect();
+
+window.addEventListener("resize",function(e){
+
+
+  rect = canvas.getBoundingClientRect();
+
+
+
+})
+
+
+
+
+
+
+ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+
+
+
+
+
+
+
 
 /* let testclick = document.getElementsByClassName("#testclick")
 
@@ -36,23 +65,94 @@ testclick.addEventListener("click", function(){
 
 testClick.forEach(element => element.addEventListener('click', event => {
 
-ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-ctx.fillStyle ="white";
-ctx.fillRect(0, 0, canvas.width, canvas.height)
-//testchange = document.getElementById("testchange")
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  //testchange = document.getElementById("testchange")
 
 
-let img =document.createElement("img");
- img.src = element.id;
 
- //testchange.setAttribute("src", element.id)
- 
-    img.onload = function(){ctx.drawImage(img ,0, 0, img.width, img.height )}
 
- 
+  let img = document.createElement("img");
+  img.src = element.id;
+
+  //testchange.setAttribute("src", element.id)
+
+  img.onload = function () { ctx.drawImage(img, 0, 0, img.width, img.height) }
+
 
 }));
+
+let x = 0
+let y = 0
+
+canvas.addEventListener("click", function(e) {
+
+  x = e.clientX - rect.left;
+  y = e.clientY - rect.top;
+  //  getMousePosition(canvas, e)
+
+
+
+    ctx.beginPath()
+  
+    ctx.fillStyle = "black"
+    ctx.fillRect(x, y, 10, 10)
+    console.log("Coordinate x: " + x,
+    "Coordinate y: " + y,
+    "page x: " + e.clientX,
+    "page y: " + e.clientY,
+    "rect left: " + rect.left,
+    "rect top: " + rect.top,
+  )
+   
+  
+
+
+
+})
+
+function drawFunction(e) {
+  x = e.clientX - rect.left;
+  y = e.clientY - rect.top;
+  //  getMousePosition(canvas, e)
+
+
+
+    ctx.beginPath()
+  
+    ctx.fillStyle = "black"
+    ctx.fillRect(x, y, 10, 10)
+    console.log("Coordinate x: " + x,
+    "Coordinate y: " + y,
+    "page x: " + e.clientX,
+    "page y: " + e.clientY,
+    "rect left: " + rect.left,
+    "rect top: " + rect.top,
+  )
+   
+  
+
+}
+
+function getMousePosition(canvas, e) {
+  let rect = canvas.getBoundingClientRect();
+  x = e.clientX - rect.left;
+  y = e.clientY - rect.top;
+  console.log("Coordinate x: " + x,
+    "Coordinate y: " + y,
+    "page x: " + e.clientX,
+    "page y: " + e.clientY);
+
+  return x, y
+}
+
+
+
+
+
+
 
 
 
