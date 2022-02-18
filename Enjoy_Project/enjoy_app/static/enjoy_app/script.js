54a5de127c560dba768c0213fb364523d0a9ecb9
travisjackson3 <travisjackson3@gmail.com>
2022-02-17 16:12:50 -0800

    undo redo erasefix

--- Enjoy_Project/enjoy_app/static/enjoy_app/script.js
+++ Enjoy_Project/enjoy_app/static/enjoy_app/script.js
@@ -6,6 +6,13 @@ let brushSize = 5
 var image = new Image();
 let eraseOn = false
 
+let imageSave = new Image
+let imageRedo = new Image
+
+let allowRedo = false
+
+//let imageSave = document.createElement("img")
+
 
 const drawPickSel = document.querySelectorAll('.drawPickSel');
 
@@ -14,8 +21,8 @@ const canvasBack = document.getElementById("canvasLayerBack");
 const ctxBack = canvasBack.getContext("2d");
 
 //draw on layer middle layer
-const canvasDraw = document.getElementById("canvasLayerDraw");
-const ctxDraw = canvasDraw.getContext("2d");
+let canvasDraw = document.getElementById("canvasLayerDraw");
+let ctxDraw = canvasDraw.getContext("2d");
 
 //cursor layer top layer
 //this layer must transfer drawing onto draw layer
@@ -23,24 +30,23 @@ const canvasCursor = document.getElementById("canvasLayerCursor");
 const ctxCursor = canvasCursor.getContext("2d");
 
 
+//save data for canvas to undo later
+let saveStateCanvass = document.createElement("canvas")
+let ctxSaveState = saveStateCanvass.getContext("2d")
+
+
 //erase
 let eraseSelect = document.getElementById("eraseSelect")
 
+//undo
+let undoSelect = document.getElementById("undoButton")
 
-eraseSelect.addEventListener("click", function(){
-  
-  if (eraseOn === true){
+//redo
+let redoSelect = document.getElementById("redoButton")
 
-    eraseOn = false
-    console.log(eraseOn)
-  }
-  else if(eraseOn === false){
-    eraseOn = true
 
-    console.log(eraseOn)
-  }
 
-})
+
 
 
 
@@ -59,6 +65,443 @@ let orangeButton = document.getElementById("orangeSelect")
 
 
 let sizeWidthDraw = document.getElementById("sizeRange")
+
+
+
+//
+// finds correct size of canvasBack based on window size.
+//display size affects accuracy of draw
+//returns correct canvasBack width and height
+//
+resizeCanvas(canvasBack)
+function resizeCanvas(canvasBack) {
+
+  let trueWidth = canvasBack.clientHeight;
+  let trueHeight = canvasBack.clientWidth;
+
+
+  if (canvasBack.width !== trueWidth || canvasBack.height !== trueHeight)
+
+
+    canvasBack.width = trueWidth;
+    canvasBack.height = trueHeight
+    canvasDraw.width = trueWidth
+    canvasDraw.height = trueHeight
+    canvasCursor.width = trueWidth;
+    canvasCursor.height = trueHeight
+
+    saveStateCanvass = trueWidth
+    saveStateCanvass = trueHeight
+
+
+
+
+}
+
+//
+//checks for window resize
+//resizes the canvasBack size and width
+//finds new position of the canvasBack
+//
+window.addEventListener("resize",function(e){
+
+  resizeCanvas(canvasBack)
+  
+ 
+  
+  })
+
+
+
+
+//Fills canvasBack with white background
+ctxBack.fillStyle = "white";
+ctxBack.fillRect(0, 0, canvasBack.width, canvasBack.height)
+
+
+
+
+//
+//Creates event to display the image on the canvasBack the user selects
+//
+drawPickSel.forEach(element => element.addEventListener('click', event => {
+
+  resizeCanvas(canvasBack)
+  data = element.dataset;
+
+  let ctxBack = canvasBack.getContext("2d");
+  ctxBack.clearRect(0, 0, canvasBack.width, canvasBack.height);
+
+  ctxBack.fillStyle = "white";
+  ctxBack.fillRect(0, 0, canvasBack.width, canvasBack.height)
+  //testchange = document.getElementById("testchange")
+
+  
+  let img = document.createElement("img");
+  img.src = data.fileloc
+
+  //testchange.setAttribute("src", element.id)
+
+  img.onload = function () { ctxBack.drawImage(img, 0, 0, img.width, img.height) }
+
+
+}));
+
+
+
+//
+//checks for change of the brush size html range input
+//
+sizeWidthDraw.addEventListener("change", function(){
+
+brushSize  = document.getElementById("sizeRange").value
+
+})
+
+
+
+
+//
+//Draw event listeners
+//
+canvasCursor.addEventListener('mousedown', e => {
+  getMousePosition(e)
+
+  isDrawing = true;
+
+  
+  let test = canvasDraw.toDataURL("image/png")
+  imageSave.src = test
+
+  undoSelect.disabled = false
+  redoSelect.disabled = true
+
+
+
+});
+
+canvasCursor.addEventListener('mousemove', e => {
+  
+  if (isDrawing === true) {
+    
+    if (eraseOn === false){
+
+      drawLine(ctxDraw, x, y, e.offsetX, e.offsetY);
+      getMousePosition(e)
+    }
+    else if(eraseOn === true){
+      ctxDraw.globalCompositeOperation="destination-out"
+      startErase(ctxDraw, x, y, e.offsetX, e.offsetY)
+      getMousePosition(e)
+      
+    ctxCursor.clearRect(0, 0, canvasCursor.width, canvasCursor.height)
+    }
+  }
+
+  
+  
+  //Adds a cursor outline, size and color indication  
+  else if (isDrawing === false) {
+  
+    ctxCursor.clearRect(0, 0, canvasCursor.width, canvasCursor.height)
+    brushOutline(ctxCursor, e.offsetX, e.offsetY)
+
+  }  
+});
+
+
+
+
+
+
+window.addEventListener('mouseup', e => {
+
+  if (isDrawing === true) {
+
+    if (eraseOn === false){
+       drawLine(ctxDraw, x, y, e.offsetX, e.offsetY);
+
+    }
+
+    else if (eraseOn === true){
+      
+      //ctxDraw.globalCompositeOperation="source-over"
+
+      startErase(ctxDraw, x, y, e.offsetX, e.offsetY)
+      ctxDraw.globalCompositeOperation="source-over"
+
+
+    }
+  }
+
+  x = 0;
+  y = 0;
+
+  isDrawing = false;
+
+});
+
+
+
+//
+//saves Draw canvas onto save state canvas
+//
+//canvasCursor.addEventListener("mouseup", function(){
+
+  //saveDrawCtx(ctxDraw)
+
+
+ // imageSave.width = canvasDraw.width
+ // imageSave.height = canvasDraw.height
+ // console.log(imageSave)
+ // imageSave = ctxSaveState.drawImage(canvasDraw, 0, 0)
+
+  //console.log(imageSave)
+  //ctxSaveState.drawImage(canvasDraw, 0, 0)
+  //imageSave =  ctxSaveState.drawImage(canvasDraw, 0, 0)
+ 
+
+
+
+  // let test = canvasDraw.toDataURL("image/png")
+
+ //   imageSave.src = test
+
+
+
+//})
+
+
+undoSelect.addEventListener("click", function(){
+
+ console.log("undo")
+
+
+ 
+// undo(ctxSaveState)
+
+
+ // ctxDraw = saveStateCanvass.getContext("2d")
+ //imageSave = ctxSaveState.drawImage(canvasDraw, 0, 0)
+ //ctxSaveState.drawImage(canvasDraw, 0, 0)
+ 
+  //test = canvasDraw.toDataURL("image/png")
+
+  //let test = canvasDraw.toDataURL("image/png")
+  let redoSave = canvasDraw.toDataURL("image/png")
+  imageRedo.src = redoSave
+
+//    imageSave.src = test
+  //document.getElementById()
+  //imgtest = test.url
+
+  //console.log(imgtest)
+
+  ctxDraw.clearRect(0, 0, canvasDraw.width, canvasDraw.height)
+
+  ctxDraw.drawImage(imageSave, 0, 0)
+
+  //let redoSave = canvasDraw.toDataURL("image/png")
+ // imageRedo.src = redoSave
+
+
+
+ //allow redo
+
+  
+
+  undoSelect.disabled = true
+  redoSelect.disabled = false
+
+
+
+  //let imgtest2 = new Image()
+  //imgtest2.src = canvasDraw.toDataURL()
+
+
+})
+
+redoSelect.addEventListener("click", function(){
+
+
+  //allow after undo has been clicked
+
+  console.log("redo")
+ 
+
+//  if (allowRedo === true){ 
+    
+  ctxDraw.clearRect(0, 0, canvasDraw.width, canvasDraw.height)
+ 
+  ctxDraw.drawImage(imageRedo, 0, 0)
+
+  undoSelect.disabled = false
+  redoSelect.disabled = true
+//  }
+
+  
+
+
+  //unallow redo click
+  //allowRedo = false
+ 
+ })
+
+
+eraseSelect.addEventListener("click", function(){
+  
+  if (eraseOn === true){
+
+    eraseOn = false
+    console.log(eraseOn)
+  }
+  else if(eraseOn === false){
+    eraseOn = true
+
+    console.log(eraseOn)
+  }
+
+  //let test = canvasDraw.toDataURL("image/png")
+  //imageSave.src = test
+
+})
+
+
+//
+//removes the cursor outline when mouse leaves canvas area
+//
+canvasCursor.addEventListener("mouseleave", function(){
+
+  ctxCursor.clearRect(0, 0, canvasCursor.width, canvasCursor.height)  
+  
+  })
+
+
+
+
+///
+///Main drawing function
+function drawLine(ctxDraw, x1, y1, x2, y2) {
+
+  ctxDraw.beginPath();
+
+  //functionality to allow erase
+  ctxDraw.globalCompositeOperation="source-over"
+
+  ctxDraw.strokeStyle = colorSelect;
+
+  ctxDraw.lineWidth = brushSize;
+  ctxDraw.lineCap = "round"
+  ctxDraw.lineJoin ="round"
+
+  ctxDraw.moveTo(x1, y1);
+  ctxDraw.lineTo(x2, y2);
+
+
+
+  ctxDraw.stroke();
+  ctxDraw.closePath();
+}
+
+
+function startErase(ctxDraw, x1, y1, x2, y2){
+
+  
+  ctxDraw.beginPath();
+  //ctxDraw.globalCompositeOperation="destination-out"
+  //ctxDraw.strokeStyle = "green";
+
+  ctxDraw.lineWidth = brushSize;
+  ctxDraw.lineCap = "round"
+  ctxDraw.lineJoin ="round"
+
+  ctxDraw.moveTo(x1, y1);
+  ctxDraw.lineTo(x2, y2);
+
+
+
+  ctxDraw.stroke();
+  ctxDraw.closePath();
+
+
+}
+
+
+//
+//makes an outline of brush size and color
+//
+function brushOutline(ctxCursor, x, y){
+
+  
+  let outlineSize = brushSize
+  
+  //color of brush
+  //lighten. lower alpha to 0.6 "alpha" hsla(hue, saturation, lightness, alpha)
+ // colorSelect = "hsla(316, 96%, 63%, 1.0)"
+
+ let currentColorSelect = colorSelect 
+ let testnew = currentColorSelect.slice(0, 20)
+ testfinal = testnew.concat("0.6)")
+
+
+  ctxCursor.beginPath();
+
+  ctxCursor.strokeStyle = colorSelect
+  ctxCursor.lineWidth = 2;
+
+  ctxCursor.arc(x, y, (outlineSize/2), 0, 2 * Math.PI);
+
+  ctxCursor.stroke();
+  ctxCursor.closePath();
+}
+
+
+//
+//gets mouse postion on canvas
+//
+function getMousePosition(e) {
+
+  x = e.offsetX
+  y = e.offsetY
+
+return x, y
+}
+
+
+//function saveDrawCtx(ctxDraw){
+  
+
+ 
+
+  //ctxSaveState.drawImage(ctxDraw.canvasDraw, 0, 0)
+
+ // saves data as url that can be accessed by visiting the URL
+  //let imageURI = canvasDraw.toDataURL()
+
+
+ // localStorage.setItem("saveState", saveData )
+
+//}
+
+//localStorage.removeItem("saveState")
+//localStorage.clear() // all data
+
+
+
+//function undo(ctxSaveState){
+  //get draw from local storage
+  //let undo = localStorage..getItem("saveSate")
+//
+ // ctxDraw = ctxSaveState
+
+  //return ctxDraw
+
+//}
+
+
+
+
+
 
 
 
@@ -127,270 +570,6 @@ orangeButton.addEventListener("click", function(e){
   
   })
            
-
-  
-
-
-
-//
-// finds correct size of canvasBack based on window size.
-//display size affects accuracy of draw
-//returns correct canvasBack width and height
-//
-resizeCanvas(canvasBack)
-function resizeCanvas(canvasBack) {
-
-  let trueWidth = canvasBack.clientHeight;
-  let trueHeight = canvasBack.clientWidth;
-
-
-  if (canvasBack.width !== trueWidth || canvasBack.height !== trueHeight)
-
-
-    canvasBack.width = trueWidth;
-    canvasBack.height = trueHeight
-    canvasDraw.width = trueWidth
-    canvasDraw.height = trueHeight
-    canvasCursor.width = trueWidth;
-    canvasCursor.height = trueHeight
-
-
-
-
-}
-
-//
-//checks for window resize
-//resizes the canvasBack size and width
-//finds new position of the canvasBack
-//
-window.addEventListener("resize",function(e){
-
-  resizeCanvas(canvasBack)
-  
-  
-  console.log(canvasBack.width)
-  console.log(canvasBack.height)
-  
-  })
-
-
-
-
-//Fills canvasBack with white background
-ctxBack.fillStyle = "white";
-ctxBack.fillRect(0, 0, canvasBack.width, canvasBack.height)
-
-
-
-
-//
-//Creates event to display the image on the canvasBack the user selects
-//
-drawPickSel.forEach(element => element.addEventListener('click', event => {
-
-  resizeCanvas(canvasBack)
-  data = element.dataset;
-
-  let ctxBack = canvasBack.getContext("2d");
-  ctxBack.clearRect(0, 0, canvasBack.width, canvasBack.height);
-
-  ctxBack.fillStyle = "white";
-  ctxBack.fillRect(0, 0, canvasBack.width, canvasBack.height)
-  //testchange = document.getElementById("testchange")
-
-  
-  let img = document.createElement("img");
-  img.src = data.fileloc
-
-  //testchange.setAttribute("src", element.id)
-
-  img.onload = function () { ctxBack.drawImage(img, 0, 0, img.width, img.height) }
-
-
-}));
-
-
-
-//
-//checks for change of the brush size html range input
-//
-sizeWidthDraw.addEventListener("change", function(){
-
-brushSize  = document.getElementById("sizeRange").value
-
-})
-
-
-
-
-
-canvasCursor.addEventListener('mousedown', e => {
-  getMousePosition(e)
-
-  isDrawing = true;
-
-});
-canvasCursor.addEventListener('mousemove', e => {
-
-  
-  if (isDrawing === true) {
-    
-    if (eraseOn === false){
-
-      drawLine(ctxDraw, x, y, e.offsetX, e.offsetY);
-      getMousePosition(e)
-    }
-    else if(eraseOn === true){
-
-      startErase(ctxDraw, x, y, e.offsetX, e.offsetY)
-      getMousePosition(e)
-      
-    ctxCursor.clearRect(0, 0, canvasCursor.width, canvasCursor.height)
-    }
-  }
-
-  //
-  //Adds a cursor outline, size and color indication
-  //
-  else if (isDrawing === false) {
-
-    ctxCursor.clearRect(0, 0, canvasCursor.width, canvasCursor.height)
-    brushOutline(ctxCursor, e.offsetX, e.offsetY)
-
-  }  
-});
-
-
-//
-//removes the cursor outline when mouse leaves canvas area
-//
-canvasCursor.addEventListener("mouseleave", function(){
-ctxCursor.clearRect(0, 0, canvasCursor.width, canvasCursor.height)
-
-
-})
-
-
-window.addEventListener('mouseup', e => {
-
-  if (isDrawing === true) {
-
-    if (eraseOn === false){
-       drawLine(ctxCursor, x, y, e.offsetX, e.offsetY);
-    }
-
-    else if (eraseOn === true){
-
-      startErase(ctxDraw, x, y, e.offsetX, e.offsetY)
-
-    }
-
-
-  }
-
-
-
-  x = 0;
-  y = 0;
-  isDrawing = false;
-
-});
-
-
-
-
-
-function drawLine(ctxDraw, x1, y1, x2, y2) {
-
-  ctxDraw.beginPath();
-  ctxDraw.globalCompositeOperation="source-over"
-
-  ctxDraw.strokeStyle = colorSelect;
-
-  ctxDraw.lineWidth = brushSize;
-  ctxDraw.lineCap = "round"
-  ctxDraw.lineJoin ="round"
-
-  ctxDraw.moveTo(x1, y1);
-  ctxDraw.lineTo(x2, y2);
-
-
-
-  ctxDraw.stroke();
-  ctxDraw.closePath();
-}
-
-
-
-//
-//makes an outline of brush size and color
-//
-function brushOutline(ctxCursor, x, y){
-
-  
-  let outlineSize = brushSize
-  
-  //color of brush
-  //lighten. lower alpha to 0.6 "alpha" hsla(hue, saturation, lightness, alpha)
- // colorSelect = "hsla(316, 96%, 63%, 1.0)"
-
- let currentColorSelect = colorSelect 
- let testnew = currentColorSelect.slice(0, 20)
- testfinal = testnew.concat("0.6)")
-
-
-  ctxCursor.beginPath();
-
-  ctxCursor.strokeStyle = colorSelect
-  ctxCursor.lineWidth = 2;
-
-  ctxCursor.arc(x, y, (outlineSize/2), 0, 2 * Math.PI);
-
-  ctxCursor.stroke();
-  ctxCursor.closePath();
-}
-
-
-
-
-function startErase(ctxDraw, x1, y1, x2, y2){
-
-  ctxDraw.beginPath();
-  ctxDraw.globalCompositeOperation="destination-out"
-  ctxDraw.strokeStyle = colorSelect;
-
-  ctxDraw.lineWidth = brushSize;
-  ctxDraw.lineCap = "round"
-  ctxDraw.lineJoin ="round"
-
-  ctxDraw.moveTo(x1, y1);
-  ctxDraw.lineTo(x2, y2);
-
-
-
-  ctxDraw.stroke();
-  ctxDraw.closePath();
-
-
-}
-
-
-
-//
-//gets mouse postion on canvas
-//
-function getMousePosition(e) {
-
-  x = e.offsetX
-  y = e.offsetY
-
-return x, y
-}
-
-
-
-
 
 
 
