09ec057366ca5ec111a8be767439ca930f786ce2
travisjackson3 <travisjackson3@gmail.com>
2022-02-22 11:21:56 -0800

    setup

--- Enjoy_Project/config/settings.py
+++ Enjoy_Project/config/settings.py
@@ -126,3 +126,7 @@ MEDIA_ROOT =""
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import django_on_heroku
+django_on_heroku.settings(locals())
